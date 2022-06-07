7e0d6ca4a79063fb701d17ce91f1e6351453dea3
Jhan Carlo Vasquez <jhancarlo.vasquez@tup.edu.ph>
2022-06-07 21:51:25 +0800

    light coral

--- MyCalculator/Form1.Designer.cs
+++ MyCalculator/Form1.Designer.cs
@@ -233,6 +233,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
             this.ClientSize = new System.Drawing.Size(342, 441);
             this.Controls.Add(this.btnEqual);
             this.Controls.Add(this.btnDivide);
